b8bd9f189189ca98733c79614e01d02e8b004c68
Hlaing Aung <betahba@gmail.com>
2022-02-01 15:40:27 -0500

    service worker

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 
 
 
-module.exports = {
+const config = {
     entry: {
         app: "./assets/js/script.js",
         events: "./assets/js/events.js",
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         filename: "[name].bundle.js",
-        path: __dirname + "/dist",
+        path: `${__dirname}/dist`
     },
     module: {
         rules: [
@@ -50,3 +50,5 @@ module.exports = {
     ],
     mode: 'development'
 };
+
+module.exports = config;
